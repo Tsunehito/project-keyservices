6d12d576970b6b85e3daea0656871b4fc74ed114
Tsunehito Higa <tsunehiga0310@gmail.com>
2020-04-06 00:12:22 +0200

    add login/logout page

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Admin, Resource, ListGuesser  } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
+import Dashboard from './dashboard/Dashboard';
+import authProvider from './auth/authProvider';
+import { PostList, PostEdit, PostCreate } from './posts/Posts';
 import { UserList } from './users/Users';
 import jsonServerProvider from 'ra-data-json-server';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
 
-  <Admin dataProvider={dataProvider} >
-    <Resource name="posts" list={ListGuesser} />
-    <Resource name="users" list={UserList} />
+  <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider} >
+    <Resource 
+      name="posts" 
+      list={PostList} 
+      edit={PostEdit} 
+      create={PostCreate}
+      icon={PostIcon}
+    />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 );
 
