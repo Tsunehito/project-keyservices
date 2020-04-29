d71d666c111d553647ab66dcfc2586b10d097344
Tsunehito Higa <tsunehiga0310@gmail.com>
2020-04-30 00:07:28 +0200

    test

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,40 @@ import React from 'react';
 import { Admin, Resource, fetchUtils } from 'react-admin';
 import Dashboard from './dashboard/Dashboard';
 import authProvider from './auth/authProvider';
+import ks_dataprovider from './dataProvider';
 import { PostList, PostEdit, PostCreate } from './posts/Posts';
 import { UserList } from './users/Users';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
-import simpleRestProvider from 'ra-data-simple-rest';
+import jsonServerProvider from 'ra-data-json-server';
+// import jsonAPIRestClient from "aor-jsonapi-client/build/restClient";
 import KSLoginPage from './login/KSLoginPage';
 
-const apiUrl = 'https://localhost:3000';
+const apiUrl = 'http://localhost:3000/api';
 const httpClient = (url, options = {}) => {
-    if (options.headers) {
+    if (!options.headers) {
         options.headers = new Headers({ Accept: 'application/json' });
     }
     const token = localStorage.getItem('accessToken');
-    options.headers.set('Authorization', `Bearer ${token}`);
+    options.headers.set('x-access-token', token);
+    options.users = {
+        authenticated: true,
+        token: token
+    };
     return fetchUtils.fetchJson(url, options);
 };
-
-const dataProvider = simpleRestProvider(apiUrl, httpClient);
+const dataProvider = jsonServerProvider(apiUrl, httpClient);
+// const dataProvider = ks_dataprovider;
+// const restClient = jsonAPIRestClient(apiUrl);
 
 const App = () => (
-  <Admin loginPage={KSLoginPage} dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider} >
+  <Admin
+      loginPage={KSLoginPage}
+      dashboard={Dashboard}
+      authProvider={authProvider}
+      dataProvider={dataProvider}
+      // restClient={restClient}
+  >
     <Resource
       name="posts" 
       list={PostList} 
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { List, Datagrid, TextField, EmailField } from 'react-admin';
-import KSUrlField from './KSUrlField';
 
 export const UserList = props => (
     <List {...props}>
         <Datagrid>
             <TextField source="id" />
-            <TextField source="name" />
+            <TextField source="lastname" />
+            <TextField source="firstname" />
             <EmailField source="email" />
-            <TextField source="phone" />
-            <KSUrlField source="website" />
-            <TextField source="company.name" />
+            <TextField source="telephone" />
+            <TextField source="adress" />
+            <TextField source="type" />
+            <TextField source="createdAt" />
+            <TextField source="updatedAt" />
         </Datagrid>
     </List>
 );
