a0d72536fc89ceea78c55be641e51147bb6cbd81
Tsune <tsunehiga0310@gmail.com>
2020-05-27 09:57:23 +0200

    add local url

--- src/config.js
+++ src/config.js
@@ -1,1 +1,5 @@
-export const apiUrl = 'http://keyservices-api.work:3000/api';
+// production
+// export const apiUrl = 'http://keyservices-api.work:3000/api';
+
+// local
+export const apiUrl = 'http://localhost:3000/api';
